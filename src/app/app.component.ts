6ad7e3397adaf1a479a382dad788c45783394f68
Clemens Basler <clemens-basler@t-online.de>
2016-12-20 12:14:49 +0100

    Buchrechner und anfang datumsrechner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
   seitenzahl = 0;
   tawLesenSchreiben = 0;
   output = 0;
   taw = 0;
   multi = 1;
+  punkteUnterhalb = 0;
 
   books = [{'value': 2, 'name': 'Folio'}, {'value': 4, 'name': 'Quarto'}, {'value': 6, 'name': 'Octavo'}];
   selectedBook = this.books[0];
@@ -31,10 +31,22 @@ export class AppComponent {
     this.calcOutput();
   }
 
-  calcOutput(){
-    this.output = this.seitenzahl / this.taw / this.multi;
+  onKey3(event:any){
+    this.punkteUnterhalb = parseInt(event.target.value);
+    this.calcOutput();
   }
+
+  calcOutput(){
+    this.output = (this.seitenzahl / this.taw / this.multi) * Math.pow(2, this.punkteUnterhalb);
+  }
+
+  zgDay = ["Markttag","Praiostag","Rohalstag","Feuertag","Wassertag","Windstag","Erdstag"];
+  zgMonth = ["Praios (1)", "Rondra (2)", "Efferd (3)", "Travia (4)", "Boron (5)", "Hesinde (6)",
+             "Firun (7)", "Tsa (8)", "Phex (9)", "Peraine (10)", "Ingerimm (11)", "Rahja (12)", "namenloser Tag (13)"];
+  selectedMonth = this.zgMonth[0];
+
+  // 1 => *2
+  // 2 => *4
+  // 3 => *8
+  // 4 => *16
 }
-
-
-// (Seitenzahl) / (TaW Lesen/Schreiben) / (2( Folio) oder 4( Quarto) oder 6( Octavo)) in Stunden
