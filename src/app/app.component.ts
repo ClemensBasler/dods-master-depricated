2a3383fa842e84559ddfaf9da8f61427ac4b5f22
Clemens Basler <clemens-basler@t-online.de>
2016-12-21 16:48:00 +0100

    datumsrechner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,13 +40,52 @@ export class AppComponent {
     this.output = (this.seitenzahl / this.taw / this.multi) * Math.pow(2, this.punkteUnterhalb);
   }
 
-  zgDay = ["Markttag","Praiostag","Rohalstag","Feuertag","Wassertag","Windstag","Erdstag"];
-  zgMonth = ["Praios (1)", "Rondra (2)", "Efferd (3)", "Travia (4)", "Boron (5)", "Hesinde (6)",
-             "Firun (7)", "Tsa (8)", "Phex (9)", "Peraine (10)", "Ingerimm (11)", "Rahja (12)", "namenloser Tag (13)"];
+  zgDay = [ "Windstag (1)", "Erdstag (2)", "Markttag (3)", "Praiostag (4)", "Rohalstag (5)", "Feuertag (6)", "Wassertag (7)"];
+  zgMonth = [{name: "Praios (1)", value: "1"},
+              {name: "Rondra (2)", value: "2"},
+              {name: "Efferd (3)", value: "3"},
+              {name: "Travia (4)", value: "4"},
+              {name: "Boron (5)", value: "5"},
+              {name: "Hesinde (6)", value: "6"},
+              {name: "Firun (7)", value: "7"},
+              {name: "Tsa (8)", value: "8"},
+              {name: "Phex (9)", value: "9"},
+              {name: "Peraine (10)", value: "10"},
+              {name: "Ingerimm (11)", value: "11"},
+              {name: "Rahja (12)", value: "12"},
+              {name: "namenloser Tag (13)", value: "13"}];
+
   selectedMonth = this.zgMonth[0];
 
-  // 1 => *2
-  // 2 => *4
-  // 3 => *8
-  // 4 => *16
+  day = 1;
+  month = 0;
+  year = 1000;
+  outputDay = "lul";
+
+
+  onKey4(event:any){
+    this.day = parseInt(event.target.value);
+    this.calcOutputLul();
+  }
+
+  onKey5(event:any){
+    this.year = parseInt(event.target.value);
+    this.calcOutputLul();
+  }
+
+  onKey6(lul){
+    this.month = parseInt(lul);
+    this.calcOutputLul();
+  }
+
+  calcOutputLul(){
+    this.outputDay = this.zgDay[(this.day + this.month*30 + 365*this.year)%7];
+  }
+
+  //1.1.1000 => Markttag
+  //2.1.1000 => Praiostag
+  //1.2.1000 => Rohalstag
+  //1.1.1001 => Praiostag
+  //1 + (1-1)*30 + 365*1000 = 365.001%7=0 => 3
+  //1 + (2-1)*30 + 365*1000 = 365.031 => 5
 }
